5653442a92217165cbd51156334fd636e12e99fc
Sagar Prabhu <48390647+sagarsp123@users.noreply.github.com>
2023-12-09 16:10:27 -0500

    Updated Node JS API

--- Backend/index.js
+++ Backend/index.js
@@ -42,6 +42,19 @@ app.get("/listings/:id", (req, res) => {
   });
 });
 
+app.get("/listings/select/:id", (req, res) => {
+  const id = req.params.id;
+  const q = "SELECT l.price, l.year, l.vehicle_id, m.MakerName, l.model, l.body_type, l.trim, l.mileage, l.sale_status, loc.addressLine1, loc.city, loc.stateAbbreviation, loc.zip5 FROM listing l JOIN makers m ON l.vehicle_id = m.vehicle_id JOIN location loc ON l.vehicle_id = loc.vehicle_id where l.vehicle_id = ?";
+
+  db.query(q,[id], (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
 app.post("/listings", (req, res) => {
   const q = "SELECT MAX(vehicle_id) AS max_vehicle_id FROM listing";
   const q1 = "SELECT MAX(maker) AS max_maker FROM listing";
@@ -93,49 +106,123 @@ app.post("/listings", (req, res) => {
             maxLoc + 1,
             maxVehicleId + 1, 
           ];
+
+          const insertMakerquery =
+          "INSERT INTO makers (MakerName, maker_id, vehicle_id) VALUES (?, ?, ?)";
+
+          const makerValues = [
+            req.body.MakerName,
+            maxMaker + 1,
+            maxVehicleId + 1, 
+          ];
     
           db.query(insertLocationQuery, locValues, (err, locationData) => {
             if (err) return res.send(err);
+
+          db.query(insertMakerquery, makerValues, (err, makerData) => {
+              if (err) return res.send(err);
   
             // Return a response indicating success
             return res.json({
               listing: listingData,
               location: locationData,
+              makers:makerData
             });
 
         //return res.json(data);
       });
+    });
     });
   });
   });
 });
 });
 
-app.delete("/books/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q = " DELETE FROM books WHERE id = ? ";
+app.delete("/listings/:id", (req, res) => {
+  const id = req.params.id;
 
-  db.query(q, [bookId], (err, data) => {
+  // Delete from the 'location' table
+  const deleteLocationQuery = "DELETE FROM location WHERE vehicle_id = ?";
+  db.query(deleteLocationQuery, [id], (err, locationData) => {
     if (err) return res.send(err);
-    return res.json(data);
+
+    // Delete from the 'makers' table
+    const deleteMakersQuery = "DELETE FROM makers WHERE vehicle_id = ?";
+    db.query(deleteMakersQuery, [id], (err, makersData) => {
+      if (err) return res.send(err);
+
+      // Delete from the 'listing' table
+      const deleteListingQuery = "DELETE FROM listing WHERE vehicle_id = ?";
+      db.query(deleteListingQuery, [id], (err, listingData) => {
+        if (err) return res.send(err);
+
+        // Return a response indicating success
+        return res.json({
+          location: locationData,
+          makers: makersData,
+          listing: listingData,
+        });
+      });
+    });
   });
 });
 
-app.put("/books/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
 
-  const values = [
-    req.body.title,
-    req.body.desc,
+app.put("/listings/:id", (req, res) => {
+  const id = req.params.id;
+
+  // Update query for the listing table
+  const updateListingQuery =
+    "UPDATE listing SET model = ?, price = ?, year = ?, body_type = ?, sale_status = ?, mileage = ?, trim = ? WHERE vehicle_id = ?";
+
+  const listingValues = [
+    req.body.model,
     req.body.price,
-    req.body.cover,
+    req.body.year,
+    req.body.body_type,
+    req.body.sale_status,
+    req.body.mileage,
+    req.body.trim,
   ];
 
-  db.query(q, [...values,bookId], (err, data) => {
+  // Perform the update for the listing table
+  db.query(updateListingQuery, [...listingValues, id], (err, listingData) => {
     if (err) return res.send(err);
-    return res.json(data);
+
+    // Update query for the location table
+    const updateLocationQuery =
+      "UPDATE location SET addressLine1 = ?, city = ?, stateAbbreviation = ?, zip5 = ? WHERE vehicle_id = ?";
+
+    const locationValues = [
+      req.body.addressLine1,
+      req.body.city,
+      req.body.stateAbbreviation,
+      req.body.zip5,
+    ];
+
+    // Perform the update for the location table
+    db.query(updateLocationQuery, [...locationValues, id], (err, locationData) => {
+      if (err) return res.send(err);
+
+    // Update query for the makers table
+    const updateMakersQuery = "UPDATE makers SET MakerName = ? WHERE vehicle_id = ?";
+
+    const makerValues = [req.body.MakerName]; // Adjust column names as per your schema
+
+    // Perform the update for the makers table
+    db.query(updateMakersQuery, [...makerValues, id], (err, makersData) => {
+      if (err) return res.send(err);
+
+
+      // Return a response indicating success
+      return res.json({
+        listing: listingData,
+        location: locationData,
+        makers: makersData
+      });
+    });
   });
+});
 });
 
 app.listen(8800, () => {
